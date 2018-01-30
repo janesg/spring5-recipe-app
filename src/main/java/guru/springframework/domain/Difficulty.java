0c5002ad9e3febb6fdc5d8f9b887c46ae2bc6948
Gary Janes <gary_janes@hotmail.com>
2018-01-30 10:24:06 +0000

    Add image controller

--- src/main/java/guru/springframework/domain/Difficulty.java
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -4,5 +4,10 @@ public enum Difficulty {
 
     EASY,
     MODERATE,
-    HARD
+    HARD;
+
+    // Added to avoid IntelliJ's issue with values() method in Thymeleaf template for drop-down
+    public static Difficulty[] getValues() {
+        return values();
+    }
 }
